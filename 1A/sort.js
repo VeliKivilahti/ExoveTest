0e585dbaa037a6aca57440b0700a07fbb080e305
Veli Kivilahti <veli.kivilahti@gmail.com>
2023-02-09 21:21:05 +0200

    Renamed variables and added console logs for node

--- 1A/sort.js
+++ 1A/sort.js
@@ -1,31 +1,34 @@
 /*
 Uses Selection Sort Algorithm
+Run node sort.js to demonstrate
 */
-let array = ["BOAT","Locomotive","Poet","Accelerate","GOLF","ACCIDENTAL","Submarine"]
-sortArray(array)
+let original = ["BOAT","Locomotive","Poet","Accelerate","GOLF","ACCIDENTAL","Submarine"]
+console.log("original array =",original)
+console.log("sorted array = ",sortArray(original))
 
 function sortArray(array){
-let newArray = array
+let sortedArray = array
 let index
 for(i = 0; i < array.length-1;i++){
     index = i
     for(j = i+1; j < array.length;j++){
-        if (newArray[j].charCodeAt(2) < newArray[index].charCodeAt(2)){
+        if (sortedArray[j].charCodeAt(2) < sortedArray[index].charCodeAt(2)){
             index = j
         }
-        if (newArray[j].charCodeAt(2) == newArray[index].charCodeAt(2)){
-            if (newArray[j].charCodeAt(1) < newArray[index].charCodeAt(1)){
+        if (sortedArray[j].charCodeAt(2) == sortedArray[index].charCodeAt(2)){
+            if (sortedArray[j].charCodeAt(1) < sortedArray[index].charCodeAt(1)){
                 index = j
             }
-            if (newArray[j].charCodeAt(1) == newArray[index].charCodeAt(1)){
-                if (newArray[j].charCodeAt(0) < newArray[index].charCodeAt(0)){
+            if (sortedArray[j].charCodeAt(1) == sortedArray[index].charCodeAt(1)){
+                if (sortedArray[j].charCodeAt(0) < sortedArray[index].charCodeAt(0)){
                     index = j  
                 }
             }
         }
     }
     if (i != index){
-    [newArray[i], newArray[index]] = [newArray[index], newArray[i]];// swap the array values 
+    [sortedArray[i], sortedArray[index]] = [sortedArray[index], sortedArray[i]];// swap the array values 
     } 
 }
+return sortedArray
 }
