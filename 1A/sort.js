970f221c2e79a093f42da45aaeef7f54c56be2bf
Veli Kivilahti <veli.kivilahti@gmail.com>
2023-02-10 12:30:32 +0200

    Refactor to make easier to change how sort it

--- 1A/sort.js
+++ 1A/sort.js
@@ -7,24 +7,22 @@ console.log("original array =",original)
 console.log("sorted array = ",sortArray(original))
 
 function sortArray(array){
-let sortedArray = array
-let index
+let sortedArray = array 
+let index, charIndex
 for(i = 0; i < array.length-1;i++){
     index = i
     for(j = i+1; j < array.length;j++){
-        if (sortedArray[j].charCodeAt(2) < sortedArray[index].charCodeAt(2)){
+        charIndex = 2
+        for (c= charIndex;sortedArray[j].charCodeAt(c) == sortedArray[index].charCodeAt(c) && c >= 0 ;c--) 
+        { // Goes through the array until characters are not the same
+            if (c !=0) { // check for that we aren't at the first / last letter that we check.
+                charIndex = c-1   // make the charIndex the next character we check
+            }
+        }
+          if (sortedArray[j].charCodeAt(charIndex) < sortedArray[index].charCodeAt(charIndex)){
             index = j
         }
-        if (sortedArray[j].charCodeAt(2) == sortedArray[index].charCodeAt(2)){
-            if (sortedArray[j].charCodeAt(1) < sortedArray[index].charCodeAt(1)){
-                index = j
-            }
-            if (sortedArray[j].charCodeAt(1) == sortedArray[index].charCodeAt(1)){
-                if (sortedArray[j].charCodeAt(0) < sortedArray[index].charCodeAt(0)){
-                    index = j  
-                }
-            }
-        }
+
     }
     if (i != index){
     [sortedArray[i], sortedArray[index]] = [sortedArray[index], sortedArray[i]];// swap the array values 
